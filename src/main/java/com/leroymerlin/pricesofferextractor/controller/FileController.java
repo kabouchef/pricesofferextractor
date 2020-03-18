70211a3ce5f8fa3ee75bdda4e21167a0522592cc
Farouk K <farouk.kabouche@ext.leroymerlin.fr>
2020-03-18 12:19:39 +0100

    :sparkles: Authentfication

--- src/main/java/com/leroymerlin/pricesofferextractor/controller/FileController.java
+++ src/main/java/com/leroymerlin/pricesofferextractor/controller/FileController.java
@@ -3,26 +3,31 @@ package com.leroymerlin.pricesofferextractor.controller;
 import com.leroymerlin.pricesofferextractor.authentification.Authentification;
 import com.leroymerlin.pricesofferextractor.authentification.UserAccess;
 import com.leroymerlin.pricesofferextractor.lmfr.SimulationOffer;
+import com.leroymerlin.pricesofferextractor.service.ExportFileService;
 import com.leroymerlin.pricesofferextractor.xmlToExcel.PriceLine;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.util.List;
 
+@Slf4j
 @RestController
+@AllArgsConstructor
 public class FileController {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
-
     public static final String API_PATH = "/api/v1";
+    private final ExportFileService exportFileService;
 
     @CrossOrigin
     @GetMapping(API_PATH + "/request")
     @ResponseBody
-    public List<PriceLine> getSimulation(@RequestParam(name = "simulationCode") String simulationCode,
-                                         @RequestParam(name = "environnement") String environnement,
-                                         @RequestParam(name = "schema") String schema) {
+    public List<PriceLine> getSimulation(@RequestParam(name = "simulationCode") String simulationCode, @RequestParam(name = "environnement") String environnement, @RequestParam(name = "schema") String schema) {
         SimulationOffer simulationOffer = new SimulationOffer();
         List<PriceLine> priceListElement = simulationOffer.getSimulationOffer(simulationCode, environnement, schema);
         return priceListElement;
@@ -31,9 +36,24 @@ public class FileController {
     @CrossOrigin
     @GetMapping(API_PATH + "/authentification")
     @ResponseBody
-    public UserAccess getAccessRight(@RequestParam(name = "idLDAP") String idLDAP){
+    public UserAccess getAccessRight(@RequestParam(name = "idLDAP") String idLDAP) {
         Authentification authentification = new Authentification();
         return authentification.getAccessRight(idLDAP);
+    }
+
+    @GetMapping(API_PATH + "/download")
+    public ResponseEntity<Resource> downloadFile(@RequestParam(name = "simulationCode") String simulationCode) {
+        File file = new File("src/main/resources/static/xls/PRICE_FROM_" + simulationCode + ".xls");
+
+        // Load file as Resource
+        Resource resource = this.exportFileService.loadFileAsResource(file.getAbsolutePath());
+
+        String contentType = "application/octet-stream";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 
