5e24b9460ddf70ed8d1fa7fd1069065a752d3f3f
Farouk K <farouk.kabouche@ext.leroymerlin.fr>
2020-03-18 12:52:35 +0100

    :bulb: Adding Swagger

--- src/main/java/com/leroymerlin/pricesofferextractor/PricesOfferExtractorApplication.java
+++ src/main/java/com/leroymerlin/pricesofferextractor/PricesOfferExtractorApplication.java
@@ -4,8 +4,10 @@ import com.leroymerlin.pricesofferextractor.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
